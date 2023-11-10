49c7d42d230aedc1f4188350e8b48f6ecb2186c3
Lera Neroznak <76168462+Lera2004@users.noreply.github.com>
2023-11-10 10:24:43 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="container">
-    <div class="map" ref="mapContainer"></div>
-    <div class="select-container">
-      <label for="smellType" class="select-label">Виберіть тип сморіду:</label>
-      <br>
-      <select id="smellType" v-model="selectedType" @change="filterMarkers" class="my-select">
-        <option value="Йод">Йод</option>
-        <option value="Аміак">Аміак</option>
-        <option value="Сірководень">Сірководень</option>
-        <option value="Сірка">Сірка</option>
-        <option value="Металургійний гар">Металургійний гар</option>
-        <option value="Горілий пластик">Горілий пластик</option>
-        <option value="Хімія">Хімія</option>
-        <option value="Гниль">Гниль</option>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <div class="map-container">
+      <div class="map" ref="mapContainer"></div>
+      <div class="legend">
+        <div class="legend-item">
+          <span class="legend-color" style="background-color: green;"></span>
+          <span class="legend-text">Здорове</span>
+        </div>
+        <div class="legend-item">
+          <span class="legend-color" style="background-color: red;"></span>
+          <span class="legend-text">Хворе</span>
+        </div>
+        <div class="legend-item">
+          <span class="legend-color" style="background-color: orange;"></span>
+          <span class="legend-text">Пошкоджене</span>
+        </div>
+        <div class="legend-item">
+          <span class="legend-color" style="background-color: blue;"></span>
+          <span class="legend-text">Молоде</span>
+        </div>
+      </div>
+      <label for="treeType" class="text-lg font-bold text-green-600 mt-4">
+        Виберіть тип дерева:
+      </label>
+      <select v-model="selectedType" @change="filterMarkers"
+        class="my-select mt-2 p-2 border border-gray-300 rounded w-1/2 md:w-1/4">
+        <option value="all">All</option>
+        <option v-for="type in uniqueTypes" :value="type" :key="type">
+          {{ type }}
+        </option>
+      </select>
+      <label for="treeState" class="text-lg font-bold text-blue-600 mt-4">
+        Виберіть стан дерева:
+      </label>
+      <select v-model="selectedState" @change="filterMarkers"
+        class="my-select mt-2 p-2 border border-gray-300 rounded w-1/2 md:w-1/4 ml-2">
+        <option value="all">All</option>
+        <option v-for="state in uniqueStates" :value="state" :key="state">
+          {{ state }}
+        </option>
       </select>
     </div>
   </div>
@@ -21,78 +47,86 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import L from "leaflet";
-import axios from "axios";
+import { treeData } from "./tree_data.js";
 
 const map = ref();
 const mapContainer = ref();
-const coordFromServer = ref([]);
 const selectedType = ref("all");
+const selectedState = ref("all");
 let markerLayer = L.layerGroup();
+
+const uniqueTypes = [...new Set(treeData.map(item => item.Tree_Name))];
+const uniqueStates = [...new Set(treeData.map(item => item.Tree_State))];
 
 const updateMarkers = () => {
   markerLayer.clearLayers();
-  const filteredData = selectedType.value === "all"
-    ? coordFromServer.value
-    : coordFromServer.value.filter(coord => coord.kind_of_smell === selectedType.value);
-  console.log("Координати обраного типу:", filteredData);
+  const filteredData = treeData.filter(item =>
+    (selectedType.value === "all" || item.Tree_Name === selectedType.value) &&
+    (selectedState.value === "all" || item.Tree_State === selectedState.value)
+  );
 
   filteredData.forEach((coordForMarker) => {
-    L.circleMarker([coordForMarker.latitude, coordForMarker.longitude], {
-      radius: 4,
-      fillColor: "green",
-      color: "green",
+    let markerColor = "green";
+
+    switch (coordForMarker.Tree_State) {
+      case "Здорове":
+        markerColor = "green";
+        break;
+      case "Хворе":
+        markerColor = "red";
+        break;
+      case "Пошкоджене":
+        markerColor = "orange";
+        break;
+      case "Молоде":
+        markerColor = "blue";
+        break;
+      default:
+        markerColor = "green";
+    }
+
+    L.circleMarker([coordForMarker.Latitude, coordForMarker.Longitude], {
+      radius: 6,
+      fillColor: markerColor,
+      color: markerColor,
     }).addTo(markerLayer);
   });
   markerLayer.addTo(map.value);
 };
 
 const filterMarkers = () => {
-   console.log("successfully");
   updateMarkers();
 };
 
-onMounted(async () => {
+onMounted(() => {
   map.value = L.map(mapContainer.value).setView([47.8388, 35.1396], 13);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map.value);
 
-  try {
-    const response = await axios.get("https://zpspace.com.ua/api/airdata");
-    coordFromServer.value = response.data;
-    updateMarkers();
-  } catch (error) {
-    console.error("Помилка отримання даних:", error);
-  }
+  updateMarkers();
 });
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
 .map {
   width: 1000px;
   height: 600px;
 }
-.my-select {
-  width: 200px;
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  font-size: 16px;
-  margin-left: 50px;
+.legend {
+  margin-left: 16px;
 }
 
-.select-label {
-  margin-left: 60px;
-  font-weight: bold;
-  font-size: 18px; 
-  color: #1e8000; 
+.legend-item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+}
+
+.legend-color {
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
 }
 </style>
